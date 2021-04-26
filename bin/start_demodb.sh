f2d34333350a9f2db1d28a4be2273e9e5061b6fb
Youngjin Joo <youngjinj@cubrid.com>
2021-04-26 13:40:50 +0900

    update

--- bin/start_demodb.sh
+++ bin/start_demodb.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cubrid service start
+VERSION=`ls -al $CUBRID_DATABASES | awk -F '/' '{printf $NF}'`
+
+if [ -d $HOME/github/backup/conf/$VERSION ]; then
+	cp -v $HOME/github/backup/conf/$VERSION/* $CUBRID/conf/	
+fi
+
+cubrid service restart
 cubrid server start demodb
