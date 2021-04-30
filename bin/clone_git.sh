f4de5377024621c782f20a065dbeee1b7eea5e98
Youngjin Joo <youngjinj@cubrid.com>
2021-04-30 17:54:46 +0900

    update

--- bin/clone_git.sh
+++ bin/clone_git.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-REPO=$1
+CLONE_NAME=$1
 
-if [ -z $REPO ]; then
+if [ -z $CLONE_NAME ]; then
 	exit
 fi
 
-if [ -e $HOME/github/$REPO ]; then
+if [ -d $HOME/github/$CLONE_NAME ]; then
 	echo "ERROR: Same name exists."
 	exit
 fi
 
 cd $HOME/github \
-&& git clone https://github.com/youngjinj/cubrid.git $REPO \
-&& cd $REPO \
-&& git remote add upstream https://github.com/CUBRID/cubrid.git \
-&& git fetch upstream \
-&& git merge upstream/develop \
-&& git remote set-url origin https://youngjinj@github.com/youngjinj/cubrid.git \
-&& git push \
-&& $HOME/bin/init_git.sh
+	&& git clone https://github.com/youngjinj/cubrid.git $CLONE_NAME \
+	&& cd $CLONE_NAME \
+	&& git remote add upstream https://github.com/CUBRID/cubrid.git \
+	&& git fetch upstream \
+	&& git merge upstream/develop \
+	&& git remote set-url origin https://youngjinj@github.com/youngjinj/cubrid.git \
+	&& git push
+
+if [[ $CLONE_NAME =~ pr-* ]]; then
+	PR_NUMBER=`echo $CLONE_NAME | awk -F "-" '{print $NF}'`
+
+	cd $HOME/github/$CLONE_NAME \
+		&& git fetch upstream pull/$PR_NUMBER/head:$CLONE_NAME \
+		&& git checkout $CLONE_NAME
+fi
+
+cd $HOME/github/$CLONE_NAME \
+	&& $HOME/bin/init_git.sh
--- bin/init_git.sh
+++ bin/init_git.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git checkout ${PWD##/*/}
+
 if [ ! -e .vscode ]; then
 	cp -r $HOME/github/backup/.vscode .
 fi
