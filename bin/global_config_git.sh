9b44502f8b1a30a5670c0562000756b66dfa3101
youngjinj <youngjinj@cubrid.com>
2021-07-16 18:05:54 +0900

    update

--- bin/global_config_git.sh
+++ bin/global_config_git.sh
@@ -3,4 +3,5 @@
 if [ `git config --list | grep "user.name" | wc -l` == 0 ] || [ `git config --list | grep "user.email" | wc -l` == 0 ]; then
         git config --global user.name "youngjinj"
         git config --global user.email "youngjinj@cubrid.com"
+	git config --global core.editor vim	
 fi
