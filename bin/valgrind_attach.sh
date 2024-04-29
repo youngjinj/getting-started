eeaa86575754d31a14a7eb28ec5c72d56efa3016
youngjinj <youngjinj@cubrid.com>
2024-04-29 16:12:05 +0900

    update

--- bin/valgrind_attach.sh
+++ bin/valgrind_attach.sh
@@ -4,10 +4,13 @@ valgrind --tool=callgrind \
          --trace-children-skip='/bin/sh,*/bin/cub_master,*/bin/cub_javasp' \
          cubrid server restart demodb
 
-csql -u dba demodb -c 'select /*+ recompile ordered */ t2.c1, t2.c2 from t1 t1, t2 t2 where t1.c1 = t2.c1 and t2.c2 = 100001'
+T_QUERY="select col3, max(col2) from t111 group by col3"
+# T_QUERY="select /*+ recompile ordered use_nl */ count(*) from t111 ta, t222 tb where ta.col1=tb.col1 and tb.col3 = 1"
+
+csql -u dba demodb -c "${T_QUERY}"
 
 callgrind_control -i on
-csql -u dba demodb -c 'select /*+ recompile ordered */ t2.c1, t2.c2 from t1 t1, t2 t2 where t1.c1 = t2.c1 and t2.c2 = 100001'
+csql -u dba demodb -c "${T_QUERY}"
 callgrind_control -i off
 
 cubrid service stop
