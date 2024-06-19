d9be5e73c57afbad885c3fc20fc3c719479a6410
youngjinj <youngjinj@cubrid.com>
2024-06-19 15:38:55 +0900

    update

--- cubrid/cubrid.sh
+++ cubrid/cubrid.sh
@@ -40,12 +40,12 @@ if [ ! -d $CUBRID_DATABASES ]; then
         mkdir -p $CUBRID_DATABASES
 fi
 
-export TMPDIR=${CUBRID}/tmp
-if [ ! -d ${TMPDIR} ]; then
-        mkdir -p ${TMPDIR}
-fi
+#export TMPDIR=${CUBRID}/tmp
+#if [ ! -d ${TMPDIR} ]; then
+#        mkdir -p ${TMPDIR}
+#fi
 
-export CUBRID_TMP=${CUBRID}/var/CUBRID_SOCK
-if [ ! -d ${CUBRID_TMP} ]; then
-        mkdir -p ${CUBRID_TMP}
-fi
+#export CUBRID_TMP=${CUBRID}/var/CUBRID_SOCK
+#if [ ! -d ${CUBRID_TMP} ]; then
+#        mkdir -p ${CUBRID_TMP}
+#fi
