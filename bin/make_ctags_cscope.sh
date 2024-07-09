d74d3edb4414c0d2c64c910118822b76c1833b9d
youngjinj <youngjinj@cubrid.com>
2024-07-09 13:00:23 +0900

    update

--- bin/make_ctags_cscope.sh
+++ bin/make_ctags_cscope.sh
@@ -6,6 +6,9 @@ if [ -z ${TARGET_PATH} ]; then
 	TARGET_PATH=${PWD}
 fi
 
+# deprecated
+exit
+
 cd ${TARGET_PATH} \
 	&& ctags -R ${TARGET_PATH} \
 	&& find ${TARGET_PATH} \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name    '*.S' \) -print > ${TARGET_PATH}/cscope.files \
