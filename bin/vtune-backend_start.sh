3c4109a38351064d0a9a59e59a2d7216c6586375
youngjinj <youngjinj@cubrid.com>
2025-08-12 00:22:52 +0900

    Add bin/vtune-backend_start.sh

---
+++ bin/vtune-backend_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /opt/intel/oneapi/setvars.sh
+
+# vtune-backend --reset-passphrase
+
+# vtune-backend --web-port=9000 --allow-remote-access --enable-server-profiling
+
+vtune-backend --web-port=9000 --enable-server-profiling &
+
+# https://127.0.0.1:9000/ui/
